2341696728473ae329dda7d16991b203d5d23d6f
Grgoirebrn <gregoire2246@gmail.com>
2024-02-29 17:14:09 +0100

    WIP added push command

--- src/commands.c
+++ src/commands.c
@@ -6,7 +6,7 @@
 /*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 21:37:11 by grebrune          #+#    #+#             */
-/*   Updated: 2024/02/12 22:54:47 by grebrune         ###   ########.fr       */
+/*   Updated: 2024/02/28 23:45:27 by grebrune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,18 @@ void	ft_swap(t_pile *lst)
 	lst->content = minus->content;
 }
 
-void	ft_push(t_pile *a, t_pile *b)
+void	ft_push(t_pile **a, t_pile **b)
 {
+	t_pile	*new;
 
+	if (!*b)
+		return ;
+	while (*b->next != NULL)
+		b = *b->next;
+	while (*a->next != NULL)
+		*a = *a->next;
+	new = malloc(sizeof (t_pile));
+	*a->next = new;
 }
 
 void	ft_rotate(t_pile *lst)
