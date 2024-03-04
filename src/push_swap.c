bd4aaa5dacb008063d5e1417025b0932ae90e9f4
Grgoirebrn <gregoire2246@gmail.com>
2024-03-04 22:43:13 +0100

    tested all need to implemente the radix

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,23 +6,85 @@
 /*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 21:29:47 by grebrune          #+#    #+#             */
-/*   Updated: 2024/02/12 21:50:17 by grebrune         ###   ########.fr       */
+/*   Updated: 2024/03/04 21:49:37 by grebrune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
+void	free_av(char **tab)
+{
+	size_t	i;
+
+	i = 0;
+	while (tab[i])
+		i++;
+	while (0 < i)
+	{
+		i--;
+		free(tab[i]);
+	}
+	free(tab);
+}
+
+void	free_list(t_pile **a)
+{
+	t_pile	*tmp;
+
+	tmp = (*a);
+	while (*a)
+	{
+		tmp = tmp->next;
+		free(*a);
+		(*a) = tmp;
+	}
+}
+
+void	print_list(t_pile **a)
+{
+	int		i;
+	t_pile	*parkour;
+
+	parkour = *a;
+	i = 0;
+	while (parkour->next != NULL)
+	{
+		ft_printf("%d = %d\n", i, parkour->content);
+		parkour = parkour->next;
+		i++;
+	}
+	ft_printf("%d = %d\n", i, parkour->content);
+}
+
 int	main(int ac, char **av)
 {
-	t_pile	a_pile;
-	char	**tab;
+	t_pile	*a_pile;
+	t_pile	*b_pile;
 
-	if (ac < 2)
-		return (0);
+	a_pile = NULL;
+	if (ac < 2 || (ac == 2 && !av[1][0]))
+		return (ft_printf("Error\nMissing Args.\n"));
 	if (ac == 2)
+		av = ft_split(av[1], ' ');
+	if (!av)
+		return (ft_printf("Error\nCrash of Malloc.\n"));
+	tab_to_pile(&a_pile, av);
+	ft_printf("TAB_TO_PILE\n");
+	free_av(av);
+	print_list(&a_pile);
+	if (!a_pile)
+		return (ft_printf("Error\nBad Input\n"), 1);
+	if (!check_sort(a_pile))
 	{
-		tab = ft_split(av[1], ' ');
-		t_pile = tab_to_pile(tab);
+		if (ft_lstsize(a_pile) == 2)
+			do_swap(&a_pile, &b_pile, 'a');
+		if (ft_lstsize(a_pile) == 3)
+			sort_three(&a_pile);
+//		else
+//			sort_algo(&a_pile, &b_pile);
 	}
+	ft_printf("FIN\n");
+	print_list(&a_pile);
+	free_list(&a_pile);
 	return (0);
 }
