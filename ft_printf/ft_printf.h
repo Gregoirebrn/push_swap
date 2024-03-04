7b0953d09e3a31e88f844a0bca174f93fa1c8007
Grgoirebrn <gregoire2246@gmail.com>
2024-03-04 22:41:17 +0100

    relocated the libft

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 19:25:27 by grebrune          #+#    #+#             */
-/*   Updated: 2023/11/30 16:00:33 by grebrune         ###   ########.fr       */
+/*   Updated: 2024/03/01 14:59:28 by grebrune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@
 # include <stdarg.h>
 # include <unistd.h>
 
-ssize_t	ft_find_type(const char c, va_list lst);
-ssize_t	ft_putnbr_b(ssize_t i, char *base, ssize_t *parse);
-ssize_t	ft_putnbr_addr(size_t i, char *base, ssize_t *parse);
-int		ft_putchar(char c);
+int	ft_printf(const char *str, ...);
+
+ssize_t		ft_find_type(const char c, va_list lst);
+ssize_t		ft_putnbr_b(ssize_t i, char *base, ssize_t *parse);
+ssize_t		ft_putnbr_addr(size_t i, char *base, ssize_t *parse);
+int	ft_putchar(char c);
 
 #endif
