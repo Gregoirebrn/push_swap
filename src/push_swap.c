479ca2111f13f528bf2f10abe901cd50de96214c
grebrune <grebrune@student.42lyon.fr>
2024-03-05 19:25:38 +0100

    no leaks are possible && norme OK

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 21:29:47 by grebrune          #+#    #+#             */
-/*   Updated: 2024/03/04 21:51:38 by grebrune         ###   ########.fr       */
+/*   Updated: 2024/03/05 19:23:48 by grebrune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,8 @@ void	free_bird(t_pile **a, char **av)
 {
 	t_pile	*tmp;
 
+	if (av)
+		free_av(av);
 	tmp = (*a);
 	while (*a)
 	{
@@ -38,7 +40,6 @@ void	free_bird(t_pile **a, char **av)
 		free(*a);
 		(*a) = tmp;
 	}
-	free_av(av);
 }
 
 void	print_list(t_pile **a)
@@ -46,6 +47,11 @@ void	print_list(t_pile **a)
 	int		i;
 	t_pile	*parkour;
 
+	if (!*a)
+	{
+		printf("list null\n");
+		return ;
+	}
 	parkour = *a;
 	i = 0;
 	while (parkour->next != NULL)
@@ -63,6 +69,7 @@ int	main(int ac, char **av)
 	t_pile	*b_pile;
 
 	a_pile = NULL;
+	b_pile = NULL;
 	if (ac < 2 || (ac == 2 && !av[1][0]))
 		return (ft_printf("Error\nMissing Args.\n"));
 	if (ac == 2)
@@ -71,17 +78,16 @@ int	main(int ac, char **av)
 		return (ft_printf("Error\nCrash of Malloc.\n"));
 	tab_to_pile(&a_pile, av);
 	if (!a_pile)
-		return (ft_printf("Error\nBad Input\n"), 1);
+		return (free_bird(&a_pile, av), ft_printf("Error\nBad Input\n"), 1);
 	if (!check_sort(a_pile))
 	{
 		if (ft_lstsize(a_pile) == 2)
 			do_swap(&a_pile, &b_pile, 'a');
 		if (ft_lstsize(a_pile) == 3)
 			sort_three(&a_pile);
-//		else
-//			sort_algo(&a_pile, &b_pile);
+		else
+			sort_algo(&a_pile, &b_pile);
 	}
-	print_list(&a_pile);
 	free_bird(&a_pile, av);
 	return (0);
 }
