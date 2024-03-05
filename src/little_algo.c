bf8976b4be7a5c87fe9d4a5d6711fb8074f66f45
grebrune <grebrune@student.42lyon.fr>
2024-03-05 21:43:47 +0100

    void * to int

--- src/little_algo.c
+++ src/little_algo.c
@@ -6,19 +6,19 @@
 /*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 19:57:47 by grebrune          #+#    #+#             */
-/*   Updated: 2024/03/04 22:32:22 by grebrune         ###   ########.fr       */
+/*   Updated: 2024/03/05 20:24:46 by grebrune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void	*biggest_nbr(t_pile **a_pile)
+int	biggest_nbr(t_pile **a_pile)
 {
-	void	*big;
+	int		big;
 	t_pile	*new;
 
 	new = (*a_pile);
-	big = NULL;
+	big = new->content;
 	while (new->next != NULL)
 	{
 		if (big < new->content)
@@ -32,7 +32,7 @@ void	*biggest_nbr(t_pile **a_pile)
 
 void	sort_three(t_pile **a_pile)
 {
-	void	*big;
+	int	big;
 
 	big = biggest_nbr(a_pile);
 	if (big == (*a_pile)->content)
