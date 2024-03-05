986de4025f7f44c2772ddbe543b00c55eecadba2
grebrune <grebrune@student.42lyon.fr>
2024-03-05 19:25:49 +0100

    added algo function

--- push_swap.h
+++ push_swap.h
@@ -6,19 +6,29 @@
 /*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 21:44:09 by grebrune          #+#    #+#             */
-/*   Updated: 2024/03/04 19:55:51 by grebrune         ###   ########.fr       */
+/*   Updated: 2024/03/05 19:12:50 by grebrune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "libftbis/libft.h"
-#include "ft_printf/ft_printf.h"
-#include <limits.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include "libftbis/libft.h"
+# include "ft_printf/ft_printf.h"
+# include <limits.h>
+
+typedef struct s_sort
+{
+	size_t	i;
+	size_t	len;
+	int		bit;
+	t_pile	*parse;
+	int		i_content;
+	int		len_b;
+}	t_sort;
 
 void	ft_swap(t_pile **lst);
 void	ft_push(t_pile **a, t_pile **b);
@@ -41,5 +51,7 @@ size_t	tab_len(char **tab);
 void	sort_algo(t_pile **a, t_pile **b);
 
 void	print_list(t_pile **a);
+
+void	free_bird(t_pile **a, char **av);
 
 #endif
